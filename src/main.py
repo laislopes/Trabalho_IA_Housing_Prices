ec31301cc11c59a840e338db8302036db56bb026
laislopes <laisjkl@hotmail.com>
2021-03-09 22:03:00 -0300

    Implements form.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from sklearn.linear_model import LinearRegression
 import pickle
 
@@ -10,6 +10,10 @@ app = Flask('housing_prices')
 @app.route('/v1')
 def healthCheck():
     return "Housing Prices API V1.0.0."
+
+@app.route('/v1/home')
+def home():
+    return render_template('index.html')
 
 @app.route('/v1/quotation', methods=['POST'])
 def quotation():
